e9900e7f1a44b48bc081869fe6a1b67bd64f3295
Oliver Turner <oliver.turner@codedsignal.co.uk>
2016-02-07 13:51:06 +0000

    Added a stateless function example: Header/SubHeader

--- app/components/Header/index.jsx
+++ app/components/Header/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import SubHeader from './subheader';
+
 /**
  * Import locally scoped styles using css-loader
  * See style.sass in this directory.
@@ -21,14 +23,7 @@ export default class Header extends React.Component {
 
       <div className={styles.wrap}>
         <h1 className={styles.title}>YARSK</h1>
-
-        <h2 className={styles.tagline}>
-          (<strong>Y</strong>et{' '}
-          <strong>A</strong>nother{' '}
-          <strong>R</strong>eact{' '}
-          <strong>S</strong>tarter{' '}
-          <strong>K</strong>it)
-        </h2>
+        <SubHeader>Yet Another React Starter Kit</SubHeader>
       </div>
     </header>;
   }
